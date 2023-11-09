a9ffd4f9ab5c935f3489737a416908f3b41f853f
Harrison <harrisonhough4@gmail.com>
2023-11-09 14:18:37 +0200

    feat: refactor and extracted transfer logic to core

--- Editor/TransferPrefab.cs
+++ Editor/TransferPrefab.cs
@@ -1,3 +1,4 @@
+using ReadyPlayerMe.Core.Editor;
 using UnityEditor;
 using UnityEngine;
 
@@ -5,6 +6,8 @@ namespace ReadyPlayerMe.PhotonSupport.Editor
 {
     public static class TransferPrefab
     {
+        private const string PREFAB_PATH = "Assets/Ready Player Me/Resources/RPM_Photon_Character.prefab";
+
         [MenuItem("Ready Player Me/Transfer Photon Prefab", false, priority = 34)]
         public static void Transfer()
         {
@@ -16,18 +19,15 @@ namespace ReadyPlayerMe.PhotonSupport.Editor
             }
             else
             {
-                if (AssetDatabase.LoadAssetAtPath("Assets/Ready Player Me/Resources/RPM_Character.prefab", typeof(GameObject)))
+                if (AssetDatabase.LoadAssetAtPath(PREFAB_PATH, typeof(GameObject)))
                 {
-                    if (!EditorUtility.DisplayDialog("Warning", "RPM_Character prefab already exists. Do you want to overwrite it?", "Yes", "No"))
+                    if (!EditorUtility.DisplayDialog("Warning", "RPM_Photon_Character prefab already exists. Do you want to overwrite it?", "Yes", "No"))
                     {
                         return;
                     }
                 }
-
-                string path = AssetDatabase.GUIDToAssetPath(guids[0]);
-                AssetDatabase.CopyAsset(path, "Assets/Ready Player Me/Resources/RPM_Character.prefab");
-                Selection.activeObject = AssetDatabase.LoadAssetAtPath("Assets/Ready Player Me/Resources/RPM_Character.prefab", typeof(GameObject));
-                Debug.Log("Photon prefab transferred to Assets/Ready Player Me/Resources/RPM_Character.prefab");
+                PrefabTransferHelper.TransferPrefabByGuid(guids[0], PREFAB_PATH);
+                Debug.Log($"Photon prefab transferred to {PREFAB_PATH}");
             }
         }
     }
