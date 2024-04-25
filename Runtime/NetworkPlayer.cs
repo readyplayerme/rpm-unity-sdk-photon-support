e4e8350b253c85c76e39da6361b15a3689d9e67d
Harrison Hough <harrisonhough4@gmail.com>
2024-04-25 08:45:29 +0300

    fix: set config before load

--- Runtime/NetworkPlayer.cs
+++ Runtime/NetworkPlayer.cs
@@ -40,13 +40,12 @@ namespace ReadyPlayerMe.PhotonSupport
         private void SetPlayer(string incomingUrl)
         {
             AvatarObjectLoader loader = new AvatarObjectLoader();
-            loader.LoadAvatar(incomingUrl);
             loader.AvatarConfig = config;
+            loader.LoadAvatar(incomingUrl);
             loader.OnCompleted += (sender, args) =>
             {
                 leftEye.transform.localPosition = AvatarBoneHelper.GetLeftEyeBone(args.Avatar.transform, true).localPosition;
                 rightEye.transform.localPosition = AvatarBoneHelper.GetRightEyeBone(args.Avatar.transform, true).localPosition;
-
                 AvatarMeshHelper.TransferMesh(args.Avatar, gameObject);
                 Destroy(args.Avatar);
             };
